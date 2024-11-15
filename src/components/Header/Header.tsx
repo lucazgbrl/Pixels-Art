9d1e6aa78735026c41632d1626bd1f567df2bd54
Lucas Gabriel <lcs.gabriel39@gmail.com>
2024-11-15 20:18:09 -0300

    components structure

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+    return (
+        <header className="app-header">
+            <h1>Pixels Art</h1>
+        </header>
+    );
+}
+
+export default Header;
